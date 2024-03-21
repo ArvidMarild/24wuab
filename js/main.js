5f1ff386ab812b114ff382cb6c594f75056170b3
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2024-03-21 13:56:06 +0100

    Dropdown menu added

--- js/main.js
+++ js/main.js
@@ -22,3 +22,14 @@ function hideMenu() {
     let layer = document.querySelector(".fade-layer");
     layer.classList.remove("visible");
 }
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      dropdownContent.classList.toggle("show-dropdown"); // Toggle the class
+    });
+  }
