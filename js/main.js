beaef27479ced97ac41032a7c036df040daf51f8
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2024-03-24 00:37:28 +0100

    read more read less

--- js/main.js
+++ js/main.js
@@ -33,3 +33,27 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.classList.toggle("show-dropdown"); // Toggle the class
     });
   }
+
+  let moreButton = document.querySelector(".read-more-button");
+  let lessButton = document.querySelector(".read-less-button");
+  let moreContent = document.getElementById("more");
+  
+  // Check if the moreContent is initially visible
+  if (moreContent.classList.contains("show")) {
+    moreButton.classList.add("hidden"); // Hide "Read More..." button
+    lessButton.classList.remove("hidden"); // Show "Read Less..." button
+  } else {
+    moreButton.classList.remove("hidden"); // Show "Read More..." button
+    lessButton.classList.add("hidden"); // Hide "Read Less..." button
+  }
+  
+  moreButton.addEventListener("click", toggleMore);
+  lessButton.addEventListener("click", toggleMore);
+  
+  function toggleMore() {
+    moreContent.classList.toggle("show"); // Toggle the class to show/hide
+  
+    // Toggle buttons
+    moreButton.classList.toggle("hidden");
+    lessButton.classList.toggle("hidden");
+  }
