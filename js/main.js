37111cf2dfcafb7ed20d383905b4aaf2941800e6
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2024-03-24 23:07:40 +0100

    dana sidan fixad (på mobil)

--- js/main.js
+++ js/main.js
@@ -1,59 +1,75 @@
 let layer = document.querySelector(".fade-layer");
-layer.addEventListener("click",showMenu)
+layer.addEventListener("click", showMenu);
 
 let button = document.querySelector(".menu-button");
-button.addEventListener("click",showMenu)
+button.addEventListener("click", showMenu);
 
 function showMenu() {
-    let menu = document.querySelector("nav.menu");
-    menu.classList.toggle("show");
+  let menu = document.querySelector("nav.menu");
+  menu.classList.toggle("show");
 
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible")
+  let layer = document.querySelector(".fade-layer");
+  layer.classList.toggle("visible");
 }
 
 let closeButton = document.getElementById("close-menu");
 closeButton.addEventListener("click", hideMenu);
 
 function hideMenu() {
-    let menu = document.querySelector("nav.menu");
-    menu.classList.remove("show");
+  let menu = document.querySelector("nav.menu");
+  menu.classList.remove("show");
 
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.remove("visible");
+  let layer = document.querySelector(".fade-layer");
+  layer.classList.remove("visible");
 }
 
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      dropdownContent.classList.toggle("show-dropdown"); // Toggle the class
-    });
-  }
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    dropdownContent.classList.toggle("show-dropdown"); // Toggle the class
+  });
+}
 
-  let moreButton = document.querySelector(".read-more-button");
-  let lessButton = document.querySelector(".read-less-button");
-  let moreContent = document.getElementById("more");
-  
-  // Check if the moreContent is initially visible
-  if (moreContent.classList.contains("show")) {
-    moreButton.classList.add("hidden"); // Hide "Read More..." button
-    lessButton.classList.remove("hidden"); // Show "Read Less..." button
-  } else {
-    moreButton.classList.remove("hidden"); // Show "Read More..." button
-    lessButton.classList.add("hidden"); // Hide "Read Less..." button
-  }
-  
-  moreButton.addEventListener("click", toggleMore);
-  lessButton.addEventListener("click", toggleMore);
-  
-  function toggleMore() {
-    moreContent.classList.toggle("show"); // Toggle the class to show/hide
-  
-    // Toggle buttons
-    moreButton.classList.toggle("hidden");
-    lessButton.classList.toggle("hidden");
-  }
+let moreButton = document.querySelector(".read-more-button");
+let lessButton = document.querySelector(".read-less-button");
+let moreContent = document.getElementById("more");
+
+// Check if the moreContent is initially visible
+if (moreContent.classList.contains("show")) {
+  moreButton.classList.add("hidden"); // Hide "Read More..." button
+  lessButton.classList.remove("hidden"); // Show "Read Less..." button
+} else {
+  moreButton.classList.remove("hidden"); // Show "Read More..." button
+  lessButton.classList.add("hidden"); // Hide "Read Less..." button
+}
+
+moreButton.addEventListener("click", toggleMore);
+lessButton.addEventListener("click", toggleMore);
+
+function toggleMore() {
+  // Toggle the class to show/hide
+  moreContent.classList.toggle("show");
+
+  // Toggle buttons
+  moreButton.classList.toggle("hidden");
+  lessButton.classList.toggle("hidden");
+}
+
+let fullSizeButtons = document.querySelectorAll(".full-size");
+fullSizeButtons.forEach((button) => {
+  button.addEventListener("click", showImg);
+});
+
+let overlayDivs = document.querySelectorAll(".overlay");
+overlayDivs.forEach((overlay) => {
+  overlay.addEventListener("click", showImg);
+});
+
+function showImg() {
+  let overlay = this.parentElement.querySelector(".overlay");
+  overlay.classList.toggle("visible");
+}
